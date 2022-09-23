4a71fa4f426c79de79570a21ec78f2fe5b8cb219
algonacci <algonacci1@gmail.com>
2022-09-23 22:15:48 +0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,52 @@
+import os
+from flask import Flask
+from flask_cors import cross_origin
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@cross_origin()
+def index():
+    return {
+        "status_code": 200,
+        "message": "Success!"
+    }
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return {
+        "status_code": 400,
+        "message": "Client side error!"
+    }, 400
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return {
+        "status_code": 404,
+        "message": "URL not found"
+    }, 404
+
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return {
+        "status_code": 405,
+        "message": "Request method not allowed!"
+    }, 405
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return {
+        "status_code": 500,
+        "message": "Server error"
+    }, 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True,
+            host="0.0.0.0",
+            port=int(os.environ.get("PORT", 8080)))
