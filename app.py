91c5f11d2907fc802e215bae49019af6aa3ce56d
algonacci <algonacci1@gmail.com>
2022-09-23 22:35:38 +0700

    Indo Summarizer

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import cross_origin
+import module as md
 
 app = Flask(__name__)
 
@@ -12,6 +13,34 @@ def index():
         "status_code": 200,
         "message": "Success!"
     }
+
+
+@app.route("/summarize", methods=["POST"])
+@cross_origin()
+def summarization():
+    input_request = request.get_json()
+    input_text = input_request["text"]
+    if request.method == "POST":
+        if input_text is None:
+            json = {
+                "data": "",
+                "message": "Input text cannot be empty",
+                "status_code": 400
+            }
+            return jsonify(json)
+        else:
+            summarized = md.summarize(text=input_text)
+            json = {
+                "result": summarized
+            }
+            return jsonify(json)
+    else:
+        json = {
+            "data": "",
+            "message": "Method not allowed",
+            "status_code": 405
+        }
+        return jsonify(json)
 
 
 @app.errorhandler(400)
---
+++ module.py
@@ -0,0 +1,23 @@
+from transformers import BertTokenizer, EncoderDecoderModel
+
+tokenizer = BertTokenizer.from_pretrained(
+    "cahya/bert2bert-indonesian-summarization")
+tokenizer.bos_token = tokenizer.cls_token
+tokenizer.eos_token = tokenizer.sep_token
+model = EncoderDecoderModel.from_pretrained(
+    "cahya/bert2bert-indonesian-summarization")
+
+
+def summarize(text):
+    input_text = tokenizer.encode(text, return_tensors='pt')
+    summary = model.generate(input_text,
+                             max_length=200,
+                             num_beams=2,
+                             repetition_penalty=2.5,
+                             length_penalty=1.0,
+                             early_stopping=True,
+                             no_repeat_ngram_size=2,
+                             use_cache=True)
+    summary_text = tokenizer.decode(summary[0], skip_special_tokens=True)
+    print(summary_text)
+    return summary_text
