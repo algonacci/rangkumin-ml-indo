0382c783bf63246daff3ec6866f2e846339ca675
algonacci <algonacci1@gmail.com>
2022-09-23 22:43:40 +0700

    refactor

--- module.py
+++ module.py
@@ -7,17 +7,19 @@ tokenizer.eos_token = tokenizer.sep_token
 model = EncoderDecoderModel.from_pretrained(
     "cahya/bert2bert-indonesian-summarization")
 
+kwargs = {
+    "max_length": 200,
+    "num_beams": 2,
+    "repetition_penalty": 5,
+    "length_penalty": 1.0,
+    "early_stopping": True,
+    "no_repeat_ngram_size": 5,
+    "use_cache": True
+}
+
 
 def summarize(text):
     input_text = tokenizer.encode(text, return_tensors='pt')
-    summary = model.generate(input_text,
-                             max_length=200,
-                             num_beams=2,
-                             repetition_penalty=2.5,
-                             length_penalty=1.0,
-                             early_stopping=True,
-                             no_repeat_ngram_size=2,
-                             use_cache=True)
+    summary = model.generate(input_text, **kwargs)
     summary_text = tokenizer.decode(summary[0], skip_special_tokens=True)
-    print(summary_text)
     return summary_text
